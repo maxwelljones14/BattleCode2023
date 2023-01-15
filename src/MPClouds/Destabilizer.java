cf9fdcf38ba6ed0cfcaa957372fb4d3dcc355718
maxwelljones14 <56135118+maxwelljones14@users.noreply.github.com>
2023-01-15 16:01:37 -0500

    added MPClouds

---
+++ src/MPClouds/Destabilizer.java
@@ -0,0 +1,18 @@
+package MPClouds;
+
+import battlecode.common.*;
+import MPClouds.Util.*;
+import MPClouds.Comms.*;
+import MPClouds.Debug.*;
+
+public class Destabilizer extends Robot {
+
+    public Destabilizer(RobotController r) throws GameActionException {
+        super(r);
+    }
+
+    public void takeTurn() throws GameActionException {
+        super.takeTurn();
+    }
+
+}
